2250d2849ac586d89d633ec3c7a29f8100e7934a
k-mcclure.1 <89980934+k-mcclSchool@users.noreply.github.com>
2023-03-30 19:10:07 -0400

    Add files via upload

---
+++ Main0.java
@@ -0,0 +1,13 @@
+public class Main0 {
+
+        public static void main(String[] args) {
+            System.out.println("Hello world!");
+            //Menu screen1 = new Menu();
+            //screen1.loadMainMenu();
+            //System.out.println("░▀▀█▀▀░█░░░░█▀▀░░░▒█▀▀▀█░█▀▀▄░█▀▀░█▀▀▀░▄▀▀▄░█▀▀▄░░░▀▀█▀▀░█▀▀▄░█▀▀▄░▀█▀░█░░");
+            //System.out.println("░░▒█░░░█▀▀█░█▀▀░░░▒█░░▒█░█▄▄▀░█▀▀░█░▀▄░█░░█░█░▒█░░░░▒█░░░█▄▄▀░█▄▄█░░█░░█░░");
+            //System.out.println("░░▒█░░░▀░░▀░▀▀▀░░░▒█▄▄▄█░▀░▀▀░▀▀▀░▀▀▀▀░░▀▀░░▀░░▀░░░░▒█░░░▀░▀▀░▀░░▀░▀▀▀░▀▀▀");
+            //int initialMoney = 1000;
+            // screen1.initialCash(initialMoney);
+        }
+    }
