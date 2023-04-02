80707a0acb319d65b05a3ca2c802c557ee9f97ff
a-casada <a-casada@onu.edu>
2023-04-02 14:44:32 -0400

    UpdateAgain

--- Party.java
+++ Party.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
+
 public class Party
 {
     private String difficulty = "EASY";
-    private String names[] = new String[6]; // five people and the pet
-    private int health[] = new int[6]; // health out of 100
-    private boolean isAlive[] = new boolean[6];
+    private String names[] = new String[4]; // five people and the pet
+    private int health[] = new int[4]; // health out of 100
+    private boolean isAlive[] = new boolean[4];
     private boolean gameOver = false;
     private boolean wonGame = false;
 
@@ -12,6 +14,10 @@ public class Party
 
     }
 
+    public String[] getNames()
+    {
+        return names;
+    }
     public boolean[] getLife()
     {
         return isAlive;
@@ -30,6 +36,19 @@ public class Party
     public void setHealth(int value[])
     {
         health = value;
+    }
+
+    /**
+     * Set Names
+     * Displays the name setting screen.
+     * Asks user for the names of the 5 traveling members.
+     * Display the names for the user
+     * Asks the user if the names are correct.
+     * @return String[5] of the names as an array.
+     */
+    public void setNames(String[] names)
+    {
+        this.names = names;
     }
 
 }
